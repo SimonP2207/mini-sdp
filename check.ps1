1f4458d55d11012ce763d3be039281e60779081e
SimonP2207 <simonp2207@gmail.com>
2026-09-23 19:35:54 +0100

    Add check.sh/ps1 for new ruff and pytest runs

---
+++ check.ps1
@@ -0,0 +1,5 @@
+Write-Output "uv run python -m pytest"
+uv run python -m pytest
+Write-Output ""
+Write-Output "uv run ruff check"
+uv run ruff check
---
+++ check.sh
@@ -0,0 +1,5 @@
+echo "uv run python -m pytest"
+uv run python -m pytest
+echo ""
+echo "uv run ruff check"
+uv run ruff check
